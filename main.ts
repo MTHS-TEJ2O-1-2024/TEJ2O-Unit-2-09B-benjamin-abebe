c65a2fe9dc07919713eaeb654dfb197261d29a82
benjamin-abebe <benjamin.abebe@mths.ca>
2024-10-09 18:36:10 -0400

    Made change in the typescript version.

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Benjamin Abebe
+ * Created on: Sep 2024
+ * This program allows users to play rock,paper,sciccor
 */
 
 let randomNumber: number = 0
